7c80e240324ba9edee5ba53a0e2a4dfd27829e1d
glasshandstudios <brandon@glasshandstudios.com>
2019-03-03 22:25:38 -0500

    new git attributes and assets

---
+++ Assets/HairDesigner/Demo/Fur/HairDesignerFurDemo2.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kalagaan
+{
+    namespace HairDesignerExtension
+    {
+        public class HairDesignerFurDemo2 : MonoBehaviour {
+
+            public HairDesigner m_hd;
+            public float m_timer = 2f;
+            public int m_id = 0;
+            float m_lastTime = 0f;
+            void Start() {
+
+                if (m_hd == null)
+                {
+                    enabled = false;
+                    return;
+                }
+
+                for (int i = 0; i < m_hd.m_generators.Count; ++i)
+                    m_hd.GetLayer(i).m_enable = i == m_id;
+            }
+
+
+            void Update()
+            {
+                if (Time.time > m_lastTime + m_timer)
+                {
+                    m_lastTime = Time.time;
+                    m_hd.GetLayer(m_id).m_enable = false;
+                    m_id++;
+                    m_id %= m_hd.m_generators.Count;
+                    m_hd.GetLayer(m_id).m_enable = true;
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/HairDesigner/Demo/HairCut/HairDesigner_Demo1.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HairDesigner_Demo1 : MonoBehaviour {
+
+    [System.Serializable]
+    public class HairCut
+    {
+        public List<string> m_layers = new List<string>();
+    }
+
+
+    public Vector3 m_rotation = new Vector3(0f,60f,0f);
+    public float m_timer = 2f;
+    public float m_speed = 10f;
+    public Kalagaan.HairDesignerExtension.HairDesigner m_hd;
+    public List<HairCut> m_hairCuts = new List<HairCut>();
+
+    Quaternion m_first;
+    Quaternion m_second;
+    bool m_switch = false;
+    float m_lastSwitchTime = 0;
+    int m_currentId = 0;
+    int m_switchCount = 0;
+
+    void Start()
+    {
+        m_first = transform.localRotation;
+        m_second = transform.localRotation* Quaternion.Euler(m_rotation);
+
+        if (m_hd == null)
+        {
+            enabled = false;
+            return;
+        }
+
+        for (int i = 0; i < m_hd.m_generators.Count; ++i)
+        {
+            m_hd.m_generators[i].m_enable = m_hairCuts[m_currentId].m_layers.Contains(m_hd.m_generators[i].m_name);         
+        }
+    }
+
+    void Update ()
+    {
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, m_switch ? m_second : m_first, Time.deltaTime * m_speed);
+        if(m_lastSwitchTime+ m_timer < Time.time )
+        {
+            m_lastSwitchTime = Time.time;
+            m_switch = !m_switch;
+            m_switchCount++;
+
+            if ( !m_switch && m_hairCuts.Count>0 && m_switchCount > 2 )
+            {
+                m_switchCount = 0;
+                m_currentId++;
+                m_currentId = m_currentId % m_hairCuts.Count;
+                for (int i = 0; i < m_hd.m_generators.Count; ++i)
+                {
+                    m_hd.m_generators[i].m_enable = m_hairCuts[m_currentId].m_layers.Contains(m_hd.m_generators[i].m_name);
+                    //if (m_hd.m_generators[i].m_enable)
+                    //    Debug.Log(m_hd.m_generators[i].m_name);
+                }
+
+            }
+        }
+    }
+}
